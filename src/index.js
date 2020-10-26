f7c42d36ceca019aa18c2a56432112e427de9db8
Walter <waltergaldamez24@gmail.com>
2020-10-26 14:04:47 -0400

    testing

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const App = () => {
+  return (
+    <div>
+    Hello, World
+    </div>
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
