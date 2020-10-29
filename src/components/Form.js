943ff774cf3859670018720add0b236295034132
Walter <waltergaldamez24@gmail.com>
2020-10-28 22:19:01 -0400

    added components and pages directories

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="username"></input>
+        <input type="password" placeholder="password"></input>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Form;
