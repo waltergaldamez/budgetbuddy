4a09112948cb5ad0c928cca923cab3d768fb4d17
brend3n <brenden@knights.ucf.edu>
2020-10-30 16:16:51 -0400

    updated MONGODB_URI

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.set('port', (process.env.PORT || 5000));
 const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
-// const url = process.env.MONGODB_URI;
-const url = 'mongodb+srv://brendenm:xdxAoJ6GBmGQk4I0@budgetbuddies.rc2gm.mongodb.net/budgetbuddiesapp?retryWrites=true&w=majority'
+const url = process.env.MONGODB_URI;
+// const url = 'mongodb+srv://brendenm:xdxAoJ6GBmGQk4I0@budgetbuddies.rc2gm.mongodb.net/budgetbuddiesapp?retryWrites=true&w=majority'
 
 const client = new MongoClient(url);
 client.connect();
@@ -29,7 +29,7 @@ app.post('/api/login', async (req, res, next) =>
     const { login, password } = req.body;
 
     const db = client.db();
-    const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+    const results = await db.collection('users').find({Login:login,Password:password}).toArray();
 
     var id = -1;
     var first = '';
