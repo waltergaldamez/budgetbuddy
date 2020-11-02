5c3fcc15764a6808caa9a5abcb6056afc74fd5b9
Walter <waltergaldamez24@gmail.com>
2020-11-01 21:35:10 -0500

    api login working

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 require('dotenv').config();
 //const url = process.env.MONGODB_URI;
-const url = 'mongodb+srv://brendenm:xdxAoJ6GBmGQk4I0@budgetbuddies.rc2gm.mongodb.net/budgetbuddiesapp?retryWrites=true&w=majority'
+const url = 'mongodb+srv://brendenm:xdxAoJ6GBmGQk4I0@budgetbuddies.rc2gm.mongodb.net/budgetbuddies?retryWrites=true&w=majority'
 
 const client = new MongoClient(url, { useUnifiedTopology: true });
 client.connect();
@@ -174,7 +174,7 @@ app.post('/api/login', async (req, res, next) =>
     }
 		else
 		{
-			ret={"email":req.param('email'), "password":req.param('password')};
+			ret={"email":req.param('email'), "passworddd":req.param('password')};
 		}
     res.status(200).json(ret);
 });
