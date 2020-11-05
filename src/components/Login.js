5d5bf4d89b5fb25df4ff2d4fdb2dd4ef0be22b65
Walter <waltergaldamez24@gmail.com>
2020-11-05 16:08:25 -0500

    More styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
-import Alert from 'Alert';
+import AlertMessage from 'AlertMessage';
 
 function Login()
 {
