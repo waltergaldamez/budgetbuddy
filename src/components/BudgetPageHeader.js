79fc84d7c1d1acb8fefebfedcd95590fda73cc62
Walter <waltergaldamez24@gmail.com>
2020-11-04 23:12:21 -0500

    add bootstrap to deps and added styling

--- src/components/BudgetPageHeader.js
+++ src/components/BudgetPageHeader.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
 
 const BudgetPageHeader = () => {
   return (
-    <h1>Budgets</h1>
+    <div>
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">BudgetBuddies</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-info">Search</Button>
+    </Form>
+  </Navbar>
+  <div>
   );
 };
 
