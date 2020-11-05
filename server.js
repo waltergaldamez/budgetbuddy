9a1c461da13c78b7c7907f046558c16d81b0f89b
Walter <waltergaldamez24@gmail.com>
2020-11-04 21:02:20 -0500

    removing profs code examples

--- server.js
+++ server.js
@@ -15,60 +15,11 @@ app.set('port', (process.env.PORT || 5000));
 const MongoClient = require('mongodb').MongoClient;
 
 
-// require('dotenv').config();
-// const url = process.env.MONGODB_URI;
-
-
-//const url = process.env.MONGODB_URI;
-
 const url = 'mongodb+srv://brendenm:xdxAoJ6GBmGQk4I0@budgetbuddies.rc2gm.mongodb.net/budgetbuddies?retryWrites=true&w=majority'
 
 const client = new MongoClient(url, { useUnifiedTopology: true });
 client.connect();
 
-
-
-
-/*-------------------START: Code from Leinecker-------------------------------*/
-// app.post('/api/addcard', async (req, res, next) =>
-// {
-//   // incoming: userId, color
-//   // outgoing: error
-
-//   var error = '';
-
-//   const { userId, card } = req.body;
-
-//   // TEMP FOR LOCAL TESTING.
-//   cardList.push( card );
-
-//   var ret = { error: error };
-//   res.status(200).json(ret);
-// });
-
-app.post('/api/searchcards', async (req, res, next) => 
-{
-  // incoming: userId, search
-  // outgoing: results[], error
-
-  var error = '';
-
-  const { userId, search } = req.body;
-
-  var _search = search.trim();
-  
-  const db = client.db();
-  const results = await db.collection('Cards').find({"Card":{$regex:_search+'.*', $options:'r'}}).toArray();
-  
-  var _ret = [];
-  for( var i=0; i<results.length; i++ )
-  {
-    _ret.push( results[i].Card );
-  }
-  
-  var ret = {results:_ret, error:error};
-  res.status(200).json(ret);
-});
 
 app.post('/api/addbudget', async (req, res, next) =>
 {
@@ -112,60 +63,6 @@ app.post('/api/register', async (req, res, next) => {
 	}
 	res.status(200).json(ret);
 });
-
-// app.post('/api/login', async (req, res, next) => 
-// {
-//   // incoming: login, password
-//   // outgoing: id, firstName, lastName, error
-
-//   var error = '';
-
-//   const { login, password } = req.body;
-
-//   var id = -1;
-//   var fn = '';
-//   var ln = '';
-
-//   if( login.toLowerCase() == 'rickl' && password == 'COP4331' )
-//   {
-//     id = 1;
-//     fn = 'Rick';
-//     ln = 'Leinecker';
-//   }
-//   else
-//   {
-//     error = 'Invalid user name/password';
-//   }
-
-//   var ret = { id:id, firstName:fn, lastName:ln, error:error};
-//   res.status(200).json(ret);
-// });
-
-// app.post('/api/searchcards', async (req, res, next) => 
-// {
-//   // incoming: userId, search
-//   // outgoing: results[], error
-
-//   var error = '';
-
-//   const { userId, search } = req.body;
-//   var _search = search.toLowerCase().trim();
-//   var _ret = [];
-
-//   for( var i=0; i<cardList.length; i++ )
-//   {
-//     var lowerFromList = cardList[i].toLocaleLowerCase();
-//     if( lowerFromList.indexOf( _search ) >= 0 )
-//     {
-//       _ret.push( cardList[i] );
-//     }
-//   }
-
-//   var ret = {results:_ret, error:''};
-//   res.status(200).json(ret);
-// });
-/*----------------------------------END: Code from Leinecker---------------------------------*/
-
 
 
 app.post('/api/login', async (req, res, next) =>
@@ -213,7 +110,7 @@ app.post('/api/findUser', async (req, res, next) =>
 
     // Query database to find user with this info
     const results = await db.collection('users').find({"username":friendUsername}).toArray();
-    
+
     // User with this info found
     if ( results.length > 0 )
     {
