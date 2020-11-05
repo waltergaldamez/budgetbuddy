fc793815d0a7a369d643df6229adbf021d032298
Walter <waltergaldamez24@gmail.com>
2020-11-05 14:49:59 -0500

    Fixing styling

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PageHeader = () => {
   return (
-    <h1 style="text-align:center">Log in To Manage Your Budget</h1>
+    <h1 style={{ text-align:'center' }}>Log in To Manage Your Budget</h1>
   );
 };
 
