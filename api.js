f9b716ef9c6e58b09a1ddceb48e9af7cbfdf7d73
brend3n <brenden@knights.ucf.edu>
2020-11-07 22:49:28 -0500

    Beginning work on showFriends

--- api.js
+++ api.js
@@ -277,4 +277,21 @@ exports.setApp = function (app, client ){
         var ret = { error: error };
         res.status(200).json(ret);
     });
+
+
+    app.post('/api/showFriends', async (req, res, next) => 
+    {
+        var error = '';
+        
+        const db = client.db();
+
+        try{
+
+        }catch(e){
+            error = e.toString();
+        }
+
+        var ret = {error:error};
+        res.status(200).json(ret);
+    });
 }
