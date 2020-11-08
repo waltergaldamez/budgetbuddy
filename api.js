628366f25ad442bbf1b1f8dacb967c453e448e69
brend3n <brenden@knights.ucf.edu>
2020-11-08 00:14:23 -0500

    Completed removeFriend

--- api.js
+++ api.js
@@ -265,10 +265,6 @@ exports.setApp = function (app, client ){
 
         try{
 
-            // If remove friends array from each user and refactor the code for
-            // add friend such that each user-friend relationship is modeled by a document in a friends collection
-            // db.collection('friends').remove({"userEmail": userID});
-
             db.collection('users').updateOne(query, update);
 
         }catch(e){
