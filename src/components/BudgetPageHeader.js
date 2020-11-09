f29a4bf5ce21959490783069aa4f156c6482cdf0
Walter <waltergaldamez24@gmail.com>
2020-11-09 11:19:24 -0500

    navbar

--- src/components/BudgetPageHeader.js
+++ src/components/BudgetPageHeader.js
@@ -7,15 +7,18 @@ const BudgetPageHeader = () => {
     <Navbar className="color-nav" variant="dark">
     <Navbar.Brand href="/budget">
       <i class="fa fa-home fa-2x circle-icon"></i>
+      <h1>Home</h1>
     </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/budget">
-      <svg width="30px" height="30px" viewBox="0 0 16 16" class="bi bi-graph-up circle-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-        <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
-      </svg>
+        <svg viewBox="0 0 16 16" class="bi bi-graph-up circle-icon" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+        <h1>Budgets</h1>
       </Nav.Link>
       <Nav.Link href="/friends">
         <i class="fa fa-trophy fa-2x circle-icon"></i>
+        <h1>Leaderboard</h1>
       </Nav.Link>
     </Nav>
     <Form inline>
