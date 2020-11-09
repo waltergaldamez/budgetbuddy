4d180f0da9b586fc47a8e35404491aac7e03e505
Walter <waltergaldamez24@gmail.com>
2020-11-09 12:02:44 -0500

    Syntax fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import BudgetsPage from './pages/BudgetsPage';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import FriendsPage from './pages/FriendsPage';
-import FriendsPage from './pages/HomePage';
+import HomePage from './pages/HomePage';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
