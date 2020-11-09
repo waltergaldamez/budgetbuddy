d05432c5d1dc63cc1a35567380de84a1e923e234
Walter <waltergaldamez24@gmail.com>
2020-11-09 00:48:46 -0500

    friendsSearchBar bootstrap

--- src/components/FriendSearchBar.js
+++ src/components/FriendSearchBar.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const SearchBar = () => {
     const [searchQuery, setSearchQuery] = useState('');
