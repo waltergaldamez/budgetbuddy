ce02167f7aacf3f7c9dfda16c4069c9a3775a34d
Walter <waltergaldamez24@gmail.com>
2020-11-09 00:13:30 -0500

    testing nav bar

--- src/components/BudgetPageHeader.js
+++ src/components/BudgetPageHeader.js
@@ -10,10 +10,10 @@ const BudgetPageHeader = () => {
     </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/budget">
-        <i class="fa fa-chart-line fa-2x circle icon"></i>
+        <i class="fa fa-chart-line fa-2x circle-icon"></i>
       </Nav.Link>
       <Nav.Link href="/friends">
-        <i class="fa fa-trophy fa-2x circle icon"></i>
+        <i class="fa fa-trophy fa-2x circle-icon"></i>
       </Nav.Link>
     </Nav>
     <Form inline>
