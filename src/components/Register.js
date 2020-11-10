b1458a563bb54ef55397dc45dfdadddd9f7f037f
Walter <waltergaldamez24@gmail.com>
2020-11-10 16:50:52 -0500

    Completing login/registration forms

--- src/components/Register.js
+++ src/components/Register.js
@@ -21,16 +21,18 @@ function Register() {
   const [ message, setMessage ] = '';
 
   const alertType = ({message}) => {
-    if (message.includes("sent"))
+    if (message === undefined )
+      return <span></span>;
+    else if (message.includes("sent"))
       return (<span class="alert-regisration"><Alert variant="success" onClose={() => setMessage('')} dismissible>
         <p>{message}</p>
       </Alert></span>);
     else if (message === "")
-      return <span></span>
+      return <span></span>;
     else
       return (<span class="alert-regisration"><Alert variant="danger" onClose={() => setMessage('')} dismissible>
         <p>{message}</p>
-      </Alert></span>)
+      </Alert></span>);
   }
 
   const doRegistration = async event => {
