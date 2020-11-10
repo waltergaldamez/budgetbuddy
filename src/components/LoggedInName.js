cdd86e3a32b00300620ce9fa433a55a4521b1761
Walter <waltergaldamez24@gmail.com>
2020-11-10 11:51:36 -0500

    fixing issues

--- src/components/LoggedInName.js
+++ src/components/LoggedInName.js
@@ -21,7 +21,7 @@ function LoggedInName()
   return(
    <div id="loggedInDiv">
    <span id="userName">Logged In As {username}</span><br />
-	 <Button variant="danger" type="submit" className="pink">
+	 <Button variant="danger" type="submit" className="pink" onClick={doLogout}>
 			<span class="material-icons black-text">
 			 exit_to_app
 			</span>
