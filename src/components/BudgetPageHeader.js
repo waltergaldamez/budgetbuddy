f4a0d8c74c865dab4fe8c6abe355cb2600022bf1
Walter <waltergaldamez24@gmail.com>
2020-11-09 23:05:02 -0500

    testing Google icons

--- src/components/BudgetPageHeader.js
+++ src/components/BudgetPageHeader.js
@@ -11,9 +11,9 @@ const BudgetPageHeader = () => {
     </Navbar.Brand>
     <Nav className="mr-auto">
       <Nav.Link href="/budget">
-        <svg viewBox="0 0 16 16" class="bi bi-graph-up circle-icon-selected" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M0 0h1v15h15v1H0V0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5z"/>
-        </svg>
+      <span class="material-icons">
+trending_up
+</span>
         <h3>Budgets</h3>
       </Nav.Link>
       <Nav.Link href="/friends">
