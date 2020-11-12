d51be3fd646a1ff2172b670fa49f009a0f241609
Walter <waltergaldamez24@gmail.com>
2020-11-12 00:07:35 -0500

    add manage friends page

---
+++ src/components/ManageFriendsPage.js
@@ -0,0 +1,40 @@
+
+const ManageFriendsPage = () ={
+  return (
+    <div>
+      <div className="upper">
+        <Container fluid>
+          <Row>
+            <Col>
+              <FriendsPageHeader />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+      <div class="lower">
+        <table class="table table-dark">
+          <thead>
+            <tr>
+              <th scope="col" class="yellow">My Friends</th>
+              <th scope="col" class="pink-header">Find New Friends <input class="friends-search"></input> <i class="fa fa-search"></i></th>
+            </tr>
+          </thead>
+          <tbody >
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Larry</td>
+            </tr>
+          </tbody>
+          </table>
+        </div>
+    </div>
+  )
+}
