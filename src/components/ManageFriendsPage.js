efdd6b70bcf130a273ec90f019b616e48c454f19
Walter <waltergaldamez24@gmail.com>
2020-11-12 00:09:24 -0500

    add manage friends page

--- src/components/ManageFriendsPage.js
+++ src/components/ManageFriendsPage.js
@@ -1,3 +1,6 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import FriendsPageHeader from '../components/FriendsPageHeader';
 
 const ManageFriendsPage = () ={
   return (
