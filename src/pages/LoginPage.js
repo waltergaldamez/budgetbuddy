1010928e0ac49cf8d299361ed56ea49d8b360be0
Walter <waltergaldamez24@gmail.com>
2020-11-12 22:36:40 -0500

    Fix NavBar and adjust/condense code

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import { Container, Row, Col } from 'react-bootstrap';
 
-
-import PageHeader from '../components/PageHeader'
 import Login from '../components/Login';
 
 const LoginPage = () => {
