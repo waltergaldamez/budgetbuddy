78ec6383985047fcdd58d9af2f92a2bbb9f9bf5a
Walter <waltergaldamez24@gmail.com>
2020-11-14 23:35:15 -0500

    adding front end folder to run locally

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ if (process.env.NODE_ENV === 'production')
 
     app.get('*', (req, res) =>
     {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     });
 }
 
