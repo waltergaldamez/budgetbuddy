7e3bc6710528608ef4a16534425c8d1be1eb24de
Zoran Kolega <kolegazoran@gmail.com>
2020-11-15 15:54:04 -0500

    Added doRecovery function to Recover.js

--- frontend/src/components/Recover.js
+++ frontend/src/components/Recover.js
@@ -8,6 +8,39 @@ function Recover() {
   var recoverPassword;
   var recoverPasswordConfirm;
 
+  const [ message, setMessage ] = useState('');
+
+  const doRecovery = async event => {
+    event.preventDefault();
+    if (recoverPassword.value.length === 0 || recoverPasswordConfirm.value.length === 0) {
+          setMessage("Please fill in all fields");
+          return;
+        }
+    if (recoverPassword.value != recoverPasswordConfirm.value) {
+      setMessage("Passwords do not match");
+      return;
+    }
+
+    var obj = {password:recoverPassword.value};
+    var js = JSON.stringify(obj);
+
+    try {
+      const response = await fetch(buildPath('api/recover'),
+            {method:'POST', body:js, headers:{'Content-Type': 'application/json'}});
+
+      var res = JSON.parse(await response.text());
+
+      if (res.error != '') {
+        setMessage(res.error);
+      } else {
+        setMessage("Password has been updated.");
+      }
+    } catch (e) {
+      alert(e.toString());
+      return;
+    }
+  }
+
   return (
     <form>
       <div className="form-group">
