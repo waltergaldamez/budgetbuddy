73dad5fa69c8eee18fef54d599234fd969e5c78a
Walter <waltergaldamez24@gmail.com>
2020-11-17 15:36:43 -0500

    Added doLogout to logout button component

--- frontend/src/components/LogOutButton.js
+++ frontend/src/components/LogOutButton.js
@@ -2,8 +2,18 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const LogOutButton = () => {
+  const doLogout = event => 
+      {
+  	    event.preventDefault();
+
+          localStorage.removeItem("username");
+  				localStorage.removeItem("userID");
+          window.location.href = '/';
+
+      };  
+
   return (
-    <Button variant="danger" type="submit" className="pink">
+    <Button variant="danger" type="submit" className="pink" onClick={doLogout}>
       <span class="material-icons">
         exit_to_app
       </span>
