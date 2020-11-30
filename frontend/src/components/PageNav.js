05584ab93738b051ee6e07c3a8e92423360aadae
Zoran Kolega <kolegazoran@gmail.com>
2020-11-30 17:14:22 -0500

    Tweaked some whitespace.

--- frontend/src/components/PageNav.js
+++ frontend/src/components/PageNav.js
@@ -4,20 +4,23 @@ import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from 'react-bootstrap'
 const PageNav = ({home, budget, leaderboard}) => {
   return (
     <Navbar className="color-nav" variant="dark">
+      {/* home */}
       <Nav.Link href="/home">
         <i class={home}></i>
         <h3 className="color-nav">Home</h3>
       </Nav.Link>
-        <Nav.Link href="/budget">
-          <span class={budget}>
-            trending_up
-          </span>
-          <h3 className="color-nav">Budgets</h3>
-        </Nav.Link>
-        <Nav.Link href="/friends" class="middle-nav">
-          <i class={leaderboard}></i>
-          <h3 className="color-nav">Leaderboard</h3>
-        </Nav.Link>
+      {/* budget */}
+      <Nav.Link href="/budget">
+        <span class={budget}>
+          trending_up
+        </span>
+        <h3 className="color-nav">Budgets</h3>
+      </Nav.Link>
+      {/* friends */}
+      <Nav.Link href="/friends" class="middle-nav">
+        <i class={leaderboard}></i>
+        <h3 className="color-nav">Leaderboard</h3>
+      </Nav.Link>
     </Navbar>
   );
 };
