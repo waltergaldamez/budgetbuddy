b43c13df23e06bfef49a08691662ed101507883c
Zoran Kolega <kolegazoran@gmail.com>
2020-11-30 22:00:42 -0500

    Added logo to login and register pages.

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Logo from '../logo.png'
+
+const EditProfile = () => {
+  return (
+    <img src={Logo} alt="Logo" className="logo"/>
+  )
+}
+
+export default EditProfile;
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import Login from '../components/Login';
+import Logo from '../components/Logo';
 
 const LoginPage = () => {
   return (
     <div className="outer">
+        <Logo />
         <div className="inner-login">
           <Login />
         </div>
