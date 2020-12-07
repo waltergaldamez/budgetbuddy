0c374f4d434f3183be557bcac306630a74ed56fd
Walter <waltergaldamez24@gmail.com>
2020-12-07 02:37:18 -0500

    allowance

--- frontend/src/components/BudgetDisplays.js
+++ frontend/src/components/BudgetDisplays.js
@@ -33,13 +33,14 @@ export default class BudgetDisplays extends React.Component {
         this.setState({
           budgets: res1.results,
           allowance: res2.allowance,
-          diff : 0
+          diff : 0,
+          index: -1, 
         })
       })
     }
 
   render() {
-    const { budgets, show, currentBudget, allowance, diff } = this.state;
+    const { budgets, show, currentBudget, allowance, diff, index } = this.state;
     var newName = '', newGoal ='';
 
     const handleClose = () => this.setState({show: false});
@@ -83,7 +84,41 @@ export default class BudgetDisplays extends React.Component {
 
   };
 
-      const remove = () => {
+  const deleteBudget2 = async event => {
+    if (event !== undefined)
+      event.preventDefault();
+    var id = budgets[event.currentTarget.getAttribute("data-index")]._id;
+    var obj = {_id: id};
+    var js = JSON.stringify(obj);
+
+        try
+        {
+        // Call to API
+
+        const response = await fetch(buildPath('api/removebudget'),
+            {method:'DELETE',body:js,headers:{'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("token")}});
+
+        // Parsing response
+            var txt = await response.text();
+            var res = JSON.parse(txt);
+
+            if( res.error.length > 0 )
+            {
+                alert( "API Error:" + res.error );
+            }
+            else
+            {
+            window.location.href = "/budget"
+            }
+        }
+        catch(e)
+        {
+            alert(e.toString());
+        }
+
+};
+
+      const remove = event => {
       handleClose();
       confirmAlert({
         title: 'Confirm to delete',
@@ -95,7 +130,7 @@ export default class BudgetDisplays extends React.Component {
           },
           {
             label: 'No',
-            onClick: () => this.setState({show: true})
+            onClick: () => this.setState({show: true, currentBudget: event.currentTarget.getAttribute("data-id")})
           }
         ]
       });
@@ -125,6 +160,7 @@ export default class BudgetDisplays extends React.Component {
               }
               else
               {
+              this.setState({index: -1});
               window.location.href = "/budget"
               }
           }
@@ -138,33 +174,32 @@ export default class BudgetDisplays extends React.Component {
       event.preventDefault();
 
       const index = event.currentTarget.getAttribute("data-index");
-      var obj = {newAmount: budgets[index].BudgetProgress,
-        _id:event.currentTarget.getAttribute("data-id")};
+      var obj = {email: localStorage.getItem("email"), funds: (parseInt(allowance) - parseInt(allowance - diff))};
       var js = JSON.stringify(obj);
+
+      const obj2 = {newAmount: budgets[index].BudgetProgress,
+        _id:event.currentTarget.getAttribute("data-id")};
+      const js2 = JSON.stringify(obj2);
 
           try
           {
           // Call to API
 
-          const response = await fetch(buildPath('api/addprogress'),
-              {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("token")}});
-
-          // Parsing response
-              var txt = await response.text();
-              var res = JSON.parse(txt);
-
-              if( res.error.length > 0 )
-              {
-                  alert( "API Error:" + res.error );
-              }
-              else
-              {
-                window.location.href = "/budget"
-              }
-          }
+          Promise.all([
+            fetch(buildPath('api/addAllowance'),
+              {method:'POST',body:js,headers:{'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("token")}}),
+              fetch(buildPath('api/addprogress'),
+              {method:'POST',body:js2,headers:{'Content-Type': 'application/json', 'Authorization': 'Bearer ' + localStorage.getItem("token")}})
+          ]).then(([res1, res2]) => {
+               return Promise.all([res1.json(), res2.json()])
+            }).then(([res1, res2]) => {
+            this.setState({index: -1});
+           window.location.href = "/budget";
+          })
+            }
           catch(e)
           {
-              alert(e.toString());
+              alert(e.toString() + "yee");
           }
     }
 
@@ -209,27 +244,36 @@ export default class BudgetDisplays extends React.Component {
                  Progress: {budget.BudgetProgress + '$'}<br/>
                 <Slider
                   axis="x"
-                  xstep={1}
+                  xstep={allowance+diff <= 0 ? 0 : 1}
                   xmin={0}
-                  xmax={budget.BudgetGoal}
+                  xmax={index == i? Math.min(budget.BudgetGoal - budget.BudgetProgress , allowance+diff + budget.BudgetProgress) + budget.BudgetProgress : budget.BudgetGoal}
                   x={budget.BudgetProgress}
-                  onChange={({ x }) => {
+                  onChange={allowance+diff <= 0 ? "" :({ x }) => {
                     budget.diff += budget.BudgetProgress - x;
                     budget.BudgetProgress = x;
-                    budget.BudgetGoal = budget.BudgetGoal;
-                    this.setState({ rerender:true, diff: budget.diff })
+                    this.setState({ rerender:true, diff: budget.diff, index: i, x: x})
                   }}
                 />
                 </div>
 
-                <Button variant="success" onClick={addProgress} data-id={budget._id} data-index={i} className="progress-submit">
+                { index == i ? <Button variant="success" onClick={addProgress} data-id={budget._id} data-index={i} className="progress-submit">
                 <span className="material-icons ">
                   check
                 </span>
                 <span className="save">
                   Save
                 </span>
-                </Button>
+                </Button> : ""}
+                {
+                  budget.BudgetGoal - budget.BudgetProgress == 0 ? <Button variant="success" onClick={deleteBudget2} data-id={budget._id} data-index={i} className="progress-submit">
+                  <span className="material-icons ">
+                    check
+                  </span>
+                  <span className="save">
+                    Mark Completed
+                  </span>
+                  </Button> : ""
+                }
 
               </div>
               <Button className="budget-edit-btn" variant="warning" onClick={handleShow} data-id={i}>
